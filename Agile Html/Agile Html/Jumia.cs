a17cfe640fe296b912bdfceb901edab05862bcb3
Winstone Were <stoniedev@gmail.com>
2020-10-17 12:56:44 +0300

    Add files via upload
    This is a simple console app whereby I'm using the HmlAgilityPack Nuget Package to web scape the common online shop in Kenya jumia.co.ke. In the program.cs file there are two methods namely SeeLaptopsFromPage which takes a page number as a string and returns a list of Laptops on that page. The second method SeeLaptopsFromName takes a name string value, queries the website and returns a list of laptops that match the name.

---
+++ Agile Html/Agile Html/Jumia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agile_Html
+{
+    class Jumia
+    {
+        static string GetLaptops(string PageNumber)
+        {
+            return "String";
+        }
+    }
+}
